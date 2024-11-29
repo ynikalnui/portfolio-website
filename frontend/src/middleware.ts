6f89d056fb6f21228959c74a85aa6f1d9b0acff3
Олег Скакун <ovskakun@gmail.com>
2024-11-29 14:22:34 +0000

    middleware disabled

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -5,11 +5,11 @@ export function middleware(req: NextRequest) {
     const url = req.nextUrl
     const { pathname } = url
 
-    if (pathname.startsWith(`/api/`)) {
-        if (!req.headers.get("referer")?.includes(process.env.NEXT_PUBLIC_APP_URL as string)) {
-            return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
-        }
-    }
+    // if (pathname.startsWith(`/api/`)) {
+    //     if (!req.headers.get("referer")?.includes(process.env.NEXT_PUBLIC_APP_URL as string)) {
+    //         return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
+    //     }
+    // }
 
     return NextResponse.next()
 }
